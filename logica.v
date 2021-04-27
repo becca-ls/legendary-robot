c2252c35bbe72a46fc508f93f2dffd25d889a6a5
yasmws <57499139+yasmws@users.noreply.github.com>
2021-04-27 03:02:44 -0300

    Create logica.v

---
+++ logica.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module logica(timer_done, door_closed, clearn, startn, stopn);
+
+input timer_done, door_closed, clearn, startn, stopn;
+
+wire A, B, S, R;
+
+assign A = (door_closed & startn & ~stopn);
+assign B = (~door_closed | timer_done | stopn | clearn);
+
+assign S = A & ~B;
+assign R = B;
+
+endmodule
