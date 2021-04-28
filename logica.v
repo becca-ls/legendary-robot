411b1b834139f4b70a2467314c4b540c14785f14
yasmws <57499139+yasmws@users.noreply.github.com>
2021-04-28 00:32:28 -0300

    mudanças feitas

--- logica.v
+++ logica.v
@@ -5,8 +5,8 @@ input timer_done, door_closed, clearn, startn, stopn;
 
 wire A, B, S, R;
 
-assign A = (door_closed & startn & ~stopn);
-assign B = (~door_closed | timer_done | stopn | clearn);
+assign A = (door_closed & ~startn & stopn);
+assign B = (~door_closed | timer_done | ~stopn | ~clearn);    
 
 assign S = A & ~B;
 assign R = B;
