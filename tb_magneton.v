db840d87228f7302d88a2c9468ab73150addca7e
yasmws <57499139+yasmws@users.noreply.github.com>
2021-04-28 00:31:24 -0300

    mudanças feitas

--- tb_magneton.v
+++ tb_magneton.v
@@ -1,3 +1,37 @@
+/*`timescale 1ns / 1ps
+
+module latch_;
+
+    wire Q, Qn;
+    reg R, S; 
+
+    latch uut(
+            .S(S), .R(R), .Q(Q), .Qn(Qn)
+    );
+    initial begin
+
+      
+        $dumpfile("ondas.vcd");  
+        $dumpvars(0, magneton_); 
+
+        S = 1; R = 1;
+        
+        #100 S = 0;
+        #100 S = 1;
+        #100 R = 0;
+        #100 R = 1;
+        #100 S = 0;
+             R = 0;
+        #100 S = 1;
+             R = 1;
+        #100 S = 0;
+             R = 0;
+        #100 ;
+    end
+
+endmodule
+
+*/
 
 `timescale 1ns / 1ps
 
@@ -15,22 +49,22 @@ module magneton_;
         $dumpfile("ondas.vcd");  
         $dumpvars(0, magneton_); 
 
-        timer_done = 0; door_closed = 0; clearn = 0; startn = 0; stopn = 0; 
+        timer_done = 0; door_closed = 0; clearn = 1; startn = 1; stopn = 1; 
 
-        timer_done = 0; door_closed = 1; clearn = 0; startn = 1; stopn = 0; 
 
+        timer_done = 0; door_closed = 1; clearn = 1; startn = 0; stopn = 1; 
         #100;
 
-        timer_done = 0; door_closed = 1; clearn = 0; startn = 0; stopn = 0;
-
+        timer_done = 0; door_closed = 1; clearn = 1; startn = 1; stopn = 1;
         #100;
 
-        timer_done = 1; door_closed = 1; clearn = 0; startn = 0; stopn = 0;
-
+        timer_done = 1; door_closed = 1; clearn = 1; startn = 1; stopn = 1;
         #100;
 
-        timer_done = 0; door_closed = 0; clearn = 0; startn = 0; stopn = 0;
+        timer_done = 0; door_closed = 0; clearn = 1; startn = 1; stopn = 1;
+        #100;
 
+        timer_done = 0; door_closed = 1; clearn = 0; startn = 0; stopn = 1;
         #100;
 
 
